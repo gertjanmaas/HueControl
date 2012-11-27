98afcd5e826a4a11feb525b7f75d5921e679e9eb
Gertjan Maas <gertjan@dragor.nl>
2012-11-27 21:29:07 +0100

    Encode light and group names used in urls

--- media/script/bridge.js
+++ media/script/bridge.js
@@ -1,10 +1,10 @@
 <%! import cherrypy %>
 function set_light(light_name, param, val)
 {
-    jQuery.ajax( '${cherrypy.url('/plugins/HueControl/control_light/')}' + light_name + '/' + param + '/' + val);
+    jQuery.ajax( '${cherrypy.url('/plugins/HueControl/control_light/')}' + encodeURIComponent(light_name) + '/' + param + '/' + val);
 }
 
 function set_group(group_name, param, val)
 {
-    jQuery.ajax( '${cherrypy.url('/plugins/HueControl/control_group/')}' + group_name + '/' + param + '/' + val);
+    jQuery.ajax( '${cherrypy.url('/plugins/HueControl/control_group/')}' + encodeURIComponent(group_name) + '/' + param + '/' + val);
 }
